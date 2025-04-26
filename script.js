ec6e176361326230bb5a2a2498b52f5344647ad4
SakshiTec <sakshichopade47@gmail.com>
2025-04-26 14:06:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Animation on Scroll
+const faders = document.querySelectorAll('.fade-up, .fade-in');
+
+const appearOptions = {
+  threshold: 0.2,
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('appear');
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
